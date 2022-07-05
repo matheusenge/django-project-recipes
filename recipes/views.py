35824ce36a2c74028db5ed83e6f7f543128222a8
Matheus Enge <matheusenge@hotmail.com>
2022-07-05 12:41:01 -0300

    header and separating the partial head

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from http.client import REQUEST_HEADER_FIELDS_TOO_LARGE
+
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
