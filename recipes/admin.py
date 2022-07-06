55bfcd3910278d4cf6ed6401e133826a8c057320
Matheus Enge <matheusenge@hotmail.com>
2022-07-06 12:24:24 -0300

    registering category in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
