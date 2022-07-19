ca6b771068bfe7b664332e6ee65bd7dc586a0023
Matheus Enge <matheusenge@hotmail.com>
2022-07-19 13:00:46 -0300

    Adding validations to the form

--- authors/forms.py
+++ authors/forms.py
@@ -1,5 +1,8 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
@@ -11,23 +14,64 @@ def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
 
 
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, one lowercase, '
+            'letter and one number. The length should be at least 8 characters.'
+        ),
+            code='Invalid'
+        )
+
+
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         add_placeholder(self.fields['username'], 'Your username')
-        add_placeholder(self.fields['email'], 'Your E-mail')
+        add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Ex.: Matheus')
         add_placeholder(self.fields['last_name'], 'Ex.: Enge')
         add_placeholder(self.fields['password'], 'Your password')
         add_placeholder(
             self.fields['confirm_password'], 'Repeat your password')
 
+    password = forms.CharField(
+        required=True, widget=forms.PasswordInput, label='Password',
+        validators=[strong_password]
+    )
+
     confirm_password = forms.CharField(
-        required=True, widget=forms.PasswordInput, label='Confirm Password')
+        required=True, widget=forms.PasswordInput, label='Confirm Password',
+    )
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
+        help_texts = {
+            'username': '',
+            'email': 'The e-mail must be valid.',
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
         widgets = {
             'password': forms.PasswordInput
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            password_confirmation_error = ValidationError(
+                'Passwords must be equal',
+                code='invalid'
+            )
+            raise ValidationError({
+                'password': password_confirmation_error,
+                'confirm_password': password_confirmation_error
+            })
